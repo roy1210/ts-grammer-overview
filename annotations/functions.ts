a358f9392b5fffc9f46ecd450f96500bdd582d2a
roy1210 <rswkure@gmail.com>
2019-08-25 09:28:26 +0800

    destructuring with annotation

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -9,3 +9,49 @@ const add = (a: number, b: number): number => {
 const subtract = (a: number, b: number): number => {
   return a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b
+}
+
+const multiply = function(a: number, b: number):
+    number {
+      return a * b
+    }
+
+// Void: Not return anything
+const logger = (message: string):
+    void => {
+      console.log(message)
+    }
+
+// Never: throw Error -> Technically, function doesn't return anything.
+// Never gonna reach to this function
+const throwError = (message: string):
+    never => {
+      throw new Error(message)
+    }
+
+const throwError2 = (message: string):
+    string => {
+      if (!message) {
+        throw new Error('Error')
+      }
+
+      return message
+    }
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny'
+}
+
+// const logWeather = (forecast: {date: Date, weather: string}): ->
+// destructuring
+const logWeather = ({date, weather}: {date: Date, weather: string}):
+    void => {
+      console.log(date)
+      console.log(weather)
+    }
+
+logWeather(forecast)
